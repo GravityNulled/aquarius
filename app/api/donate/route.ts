a9753504f1c51efa38dc4f72664747b69da2a318
Omar <annexomar@gmail.com>
2023-05-29 20:10:02 +0300

    feat: donation-api-endpoint

--- app/api/donate/route.ts
+++ app/api/donate/route.ts
@@ -1,15 +1,24 @@
+import prisma from "@/utils/client";
+import { Donation } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const body: IDonation = await request.json();
+  const body: Donation = await request.json();
   const { name, email, phone, date, age, gender, weight } = body;
-  return NextResponse.json<IDonation>({
-    name,
-    email,
-    phone,
-    date,
-    age,
-    gender,
-    weight,
-  });
+  try {
+    const donationRequest = await prisma.donation.create({
+      data: {
+        age,
+        date,
+        email,
+        gender,
+        name,
+        phone,
+        weight,
+      },
+    });
+    return NextResponse.json<Donation>(donationRequest, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "server error" }, { status: 500 });
+  }
 }
