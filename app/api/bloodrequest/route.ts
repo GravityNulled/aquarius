54c7920f282970ac9ecfa774cdd2e5a0c1d0747f
Omar <annexomar@gmail.com>
2023-05-29 19:56:58 +0300

    feat: backend-B-request

--- app/api/bloodrequest/route.ts
+++ app/api/bloodrequest/route.ts
@@ -1,14 +1,24 @@
+import prisma from "@/utils/client";
+import { BloodRequest } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const body: IBloodRequest = await request.json();
+  const body: BloodRequest = await request.json();
   const { name, email, phone, date, group, reason } = body;
-  return NextResponse.json<IBloodRequest>({
-    name,
-    email,
-    phone,
-    date,
-    group,
-    reason,
-  });
+  try {
+    const bloodrequest = await prisma.bloodRequest.create({
+      data: {
+        name,
+        email,
+        date,
+        group,
+        phone,
+        reason,
+      },
+    });
+    console.log(bloodrequest);
+    return NextResponse.json(bloodrequest);
+  } catch (error) {
+    return NextResponse.json({ message: "error" });
+  }
 }
