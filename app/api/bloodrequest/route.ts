2c6e2c1dc71334cbed16e9ed4ca0d4881fa611dd
Omar <annexomar@gmail.com>
2023-05-29 20:33:23 +0300

    feat: request-patch-endpoint

--- app/api/bloodrequest/route.ts
+++ app/api/bloodrequest/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/utils/client";
 import { BloodRequest } from "@prisma/client";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function POST(request: Request) {
   const body: BloodRequest = await request.json();
@@ -19,5 +21,28 @@ export async function POST(request: Request) {
     return NextResponse.json(bloodrequest, { status: 500 });
   } catch (error) {
     return NextResponse.json({ message: "server error" }, { status: 500 });
+  }
+}
+
+export async function PATCH(request: Request) {
+  const session = await getServerSession(authOptions);
+  if (session != null) {
+    const body: BloodRequest = await request.json();
+    const { id, status } = body;
+    try {
+      const updateBloodRequest = await prisma.bloodRequest.update({
+        where: {
+          id,
+        },
+        data: {
+          status: status,
+        },
+      });
+      return NextResponse.json(updateBloodRequest, { status: 204 });
+    } catch (error) {
+      return NextResponse.json({ message: "server error" }, { status: 500 });
+    }
+  } else {
+    return NextResponse.json({ message: "Forbidden" }, { status: 403 });
   }
 }
