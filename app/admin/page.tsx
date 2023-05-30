b7c5a58d3d146b8a924f786601c27fcb60aae918
Omar <annexomar@gmail.com>
2023-05-30 13:34:29 +0300

    feat: added appointments

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -10,14 +10,22 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-import { BloodRequest } from "@prisma/client";
+import { BloodRequest, Donation } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import { format } from "date-fns";
 
 const getdata = async () => {
   const response = await fetch("http://localhost:3000/api/bloodrequest", {
-    cache: "force-cache",
+    cache: "no-store",
+  });
+  const res = await response.json();
+  return res;
+};
+const getAppointments = async () => {
+  const response = await fetch("http://localhost:3000/api/donate", {
+    cache: "no-store",
   });
   const res = await response.json();
   return res;
@@ -30,6 +38,8 @@ const Admin = async () => {
     redirect("/");
   }
   const requestInfo: BloodRequest[] = await getdata();
+  const appointments: Donation[] = await getAppointments();
+
   return (
     <div className="mt-10 container w-full p-4 md:w-5/6 mx-auto">
       <h1 className="text-3xl font-medium mb-5 text-center uppercase">
@@ -52,8 +62,8 @@ const Admin = async () => {
           {requestInfo?.map((request) => (
             <TableRow key={request.id}>
               <TableCell className="font-medium">{request.name}</TableCell>
-              <TableCell>{request.phone}</TableCell>
               <TableCell>{request.reason}</TableCell>
+              <TableCell>{request.phone}</TableCell>
               <TableCell className="text-right">{request.status}</TableCell>
               <TableCell>
                 <Modal
@@ -71,6 +81,38 @@ const Admin = async () => {
           ))}
         </TableBody>
       </Table>
+      <div className="mt-20">
+        <p className="text-center mb-5 font-semibold">All Appointments</p>
+        <Table className="w-3/4 mx-auto">
+          <TableCaption>A list of All Appointments made.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Appointee</TableHead>
+              <TableHead>Phone</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead className="text-right">Gender</TableHead>
+              <TableHead className="text-right">Time</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {appointments?.map((appointment) => (
+              <TableRow key={appointment.id}>
+                <TableCell className="font-medium">
+                  {appointment.name}
+                </TableCell>
+                <TableCell>{appointment.phone}</TableCell>
+                <TableCell>{appointment.email}</TableCell>
+                <TableCell className="text-right">
+                  {appointment.gender}
+                </TableCell>
+                <TableCell className="text-right">
+                  {format(new Date(appointment.date), "dd/MM/yyyy")}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
